a836de48fcdbe155215e1054a334ee349be3f476
Giordano Berwanger <giordano-pydd@hotmail.com>
2017-06-12 20:00:32 -0300

    Create main.c

--- Lista Estática/main.c
+++ Lista Estática/main.c
@@ -1,3 +1,10 @@
+/*
+ ============================================================================
+ Arquivo     : Lista Estática (main.c)
+ Autor       : Giordano Berwanger (Cryogenio)
+ ============================================================================
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
